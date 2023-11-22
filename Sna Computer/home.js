65393fe2154ce69a4761d255c2836080160110ac
Ron Veasna <vaing133@gmail.com>
2023-11-22 19:50:33 +0700

    New Update

---
+++ Sna Computer/home.js
@@ -0,0 +1,12 @@
+function s(){
+    alert("We have not input data for Searching yet")
+}
+function buy(){
+    alert("Sorry you can not buy here, please come to our store at the place.")
+}
+function com(){
+    alert("Here we have only some PC on the web page!")
+}
+function acc(){
+    alert("Hi, could you come to our store bcuz, we haven't input data of accessory yet!")
+}
